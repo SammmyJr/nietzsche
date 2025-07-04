a327738df91a097073e4f08bc2be28153c7b48f5
Samuel <culleys1@wwstu.catholic.edu.au>
2025-07-05 07:30:06 +1000

    Attempting to make Entity classes & integrations

--- Classes/ExtendedWindow.gd
+++ Classes/ExtendedWindow.gd
@@ -2,9 +2,13 @@ class_name ExtendedWindow
 extends Window
 
 @export var startPositionOffset: Vector2i = Vector2i.ZERO
+@export var windowScaleFactor: float = 1.0
+
 @onready var stateManger: StateManager = get_node("/root/Explorer/State Manager")
 @onready var encounterManger: EncounterManager = get_node("/root/Explorer/Encounter Manager")
 
 
 func _ready():
 	get_window().position += startPositionOffset
+	get_window().content_scale_factor = windowScaleFactor
+	get_window().size = get_window().size * windowScaleFactor
---
+++ Classes/entityResource.gd
@@ -0,0 +1,5 @@
+class_name EntityResource
+extends Node
+
+@export var username: String = "Bob"
+@export var hp: int = 10
